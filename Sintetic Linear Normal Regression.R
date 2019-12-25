6d4b6c5ba15ae14a31ecfac4c6931432360be33f
fbetteo <francobetteo@gmail.com>
2019-12-25 18:32:27 -0300

    Generate Linear Relationship

---
+++ Sintetic Linear Normal Regression.R
@@ -0,0 +1,34 @@
+# Snippet to generate linear relationships. Variance can be constant or variable. 
+
+library(ggplot2)
+
+set.seed(1)
+
+# parameters ----
+n = 1000
+b0 = 10
+b1 = 3
+x1 = runif(n, 1, 20)
+constant_variance = 25
+unconstant_variance_structure = 1 + 3*x1
+
+
+# linear regression. Fixed SD. ----
+# Equivalents
+y = b0 + b1*x1 + rnorm(n, 0, sqrt(constant_variance))
+y =  MASS::mvrnorm(n = 1, mu = b0 + b1*x1, Sigma = diag(constant_variance,1000,1000))
+
+# linear regression. SD depends on X1 ----
+y =  MASS::mvrnorm(n = 1, mu = b0 + b1*x1, Sigma = diag(unconstant_variance_structure))
+
+# DF ----
+df = data.frame(y, x1)
+
+# check ----
+ggplot( data  = df, aes( x = x1, y = y)) + 
+  geom_point()
+
+reg= lm(y ~ x1, data = df)
+sd(reg$residuals)
+summary(reg)
+
