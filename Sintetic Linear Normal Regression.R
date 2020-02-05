47556cb9468d8cd0ec9f5ad3983b9ed41dc37396
Franco <francobetteo@gmail.com>
2020-02-04 23:19:53 -0300

    minor fix

--- Sintetic Linear Normal Regression.R
+++ Sintetic Linear Normal Regression.R
@@ -16,7 +16,7 @@ unconstant_variance_structure = 1 + 3*x1
 # linear regression. Fixed SD. ----
 # Equivalents
 y = b0 + b1*x1 + rnorm(n, 0, sqrt(constant_variance))
-y =  MASS::mvrnorm(n = 1, mu = b0 + b1*x1, Sigma = diag(constant_variance,1000,1000))
+y =  MASS::mvrnorm(n = 1, mu = b0 + b1*x1, Sigma = diag(constant_variance, n , n))
 
 # linear regression. SD depends on X1 ----
 y =  MASS::mvrnorm(n = 1, mu = b0 + b1*x1, Sigma = diag(unconstant_variance_structure))
